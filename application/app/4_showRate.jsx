2cf8236ea620bed1509abb1901ab256488b74a9f
NorbertZdziarski <norbertzdz@gmail.com>
2023-10-26 13:27:56 +0200

    add show rate

--- application/app/4_showRate.jsx
+++ application/app/4_showRate.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
 function ShowRate({rateArr}) {
-    const quantity = rateArr.length;
-    let rateValue = 5;
-
+    let rateValue = 0;
+    if (rateArr) {
+        let newRateArr = []
+        console.log(rateArr)
+        rateArr.map((rate)=>{newRateArr.push(rate.rate)})
+        const quantity = rateArr.length;
+        console.log(newRateArr);
+        console.log(quantity);
+        const sum = newRateArr.reduce((a, b) => a + b, 0)
+        rateValue =  Math.round(sum / quantity);
+    }
     return (
         <p>{'*'.repeat(rateValue)}</p>
     )
