9fff8bb58d11ff5f430a78bd04b0a498a886449b
NorbertZdziarski <norbertzdz@gmail.com>
2023-11-20 10:05:03 +0100

    user panel design

--- application/app/5_showPhoto-slide.jsx
+++ application/app/5_showPhoto-slide.jsx
@@ -1,5 +1,7 @@
 import LoadImage from "./a_loadimage";
 import React, {useState} from "react";
+import btnImgL from '../images/1904658-arrow-arrow-left-change-direction-left-next-undo_122508.png'
+import btnImgR from '../images/1904671-arrow-arrow-right-change-direction-next-page-right_122521.png'
 
 function ShowPhotoSlide({photo, style, photoPath='images/trips'}) {
 
@@ -20,11 +22,11 @@ function ShowPhotoSlide({photo, style, photoPath='images/trips'}) {
         }
 
     return (<div className="imageBox" >
-        <button onClick={fnBtnLeft} className="imageBox_btn_L"> lewo </button>
+        <button onClick={fnBtnLeft} className="imageBox_btn_L"> <img src={btnImgL} className="btnarrow"/> </button>
         <LoadImage imageName={photo[photoNr]}
                    imagePath={photoPath}
                    photoClass={style} />
-        <button onClick={fnBtnRight} className="imageBox_btn_R"> prawo</button>
+        <button onClick={fnBtnRight} className="imageBox_btn_R"> <img src={btnImgR} className="btnarrow"/> </button>
     </div>)
 }
 
